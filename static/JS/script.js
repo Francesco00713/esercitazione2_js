afb40610809437a4249b1962298e3bd7ccb0ce2f
Domengius_20-11 <kiccofranca13@gmail.com>
2024-10-08 09:50:21 +0200

    Prova

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,26 +1,33 @@
-function generateRandomInteger(max){
-    n = Math.floor(Math.random() * max);
-    return n; 
+function generateRandomInteger(min = 0,  max = 100){
+    let number = Math.random()
+    number = number * (max - min) + min
+    return Math.floor(number)
+}
+
+function handleClick(event){ 
+    console.log("Benvenuto al gioco!");
+    while(tentativi < 5){
+        do {
+            userField = document.querySelector("#guess");
+            btn = document.querySelector("#guessBtn");
+        } while (userField === null || isNaN(userField)); 
+
+        userField = parseInt(userField);
+        tentativi++;
+    
+        if(userField === n){
+            console.log("Complimenti hai vinto!");
+        } else if (userField < n){
+            console.log(userField + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi.");
+        } else if(userField > n){
+            console.log(userField + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi.");
+        }
+    }
+    console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
 }
 
 let n = 0, tentativi = 0, indovinare;
+let userField, btn;
 n = generateRandomInteger(100);
 
-console.log("Benvenuto al gioco!");
-while(tentativi < 5){
-    do {
-        indovinare = prompt("Inserisci il numero da indovinare: ");
-    } while (indovinare === null || isNaN(indovinare)); 
-
-    indovinare = parseInt(indovinare);
-    tentativi++;
-    
-    if(indovinare === n){
-        console.log("Complimenti hai vinto!");
-    } else if (indovinare < n){
-        console.log(indovinare + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi.");
-    } else if(indovinare > n){
-        console.log(indovinare + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi.");
-    }
-}
-console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
+guessBtn.addEventListener("click", handleClick);
