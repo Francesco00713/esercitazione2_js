2b62f148da489adc8cffd165aa98ebf7c4ae7344
Francesco00713 <kiccofranca13@gmail.com>
2024-10-12 19:18:34 +0200

    Gioco corretto e funzionante ma privo di codicecss

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,33 +1,40 @@
 function generateRandomInteger(min = 0,  max = 100){
     let number = Math.random()
-    number = number * (max - min) + min
+    number = number * (max - min + 1) + min
     return Math.floor(number)
 }
 
-function handleClick(event){ 
-    console.log("Benvenuto al gioco!");
-    while(tentativi < 5){
-        do {
-            userField = document.querySelector("#guess");
-            btn = document.querySelector("#guessBtn");
-        } while (userField === null || isNaN(userField)); 
+let tentativi = 0;
+const n = generateRandomInteger(0, 100);
 
-        userField = parseInt(userField);
-        tentativi++;
-    
-        if(userField === n){
-            console.log("Complimenti hai vinto!");
-        } else if (userField < n){
-            console.log(userField + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi.");
-        } else if(userField > n){
-            console.log(userField + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi.");
-        }
+function handleClick(event){
+
+
+    let userField = document.querySelector("#guess");
+
+    if(userField === null || isNaN(userField.value) || userField.value === ""){
+        console.log("Devi inserire un numero valido!");
+        return;
     }
-    console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
+
+    let indovino = parseInt(userField.value);
+    tentativi++;
+
+    if(indovino === n){
+        console.log("Complimenti hai vinto!");
+        return;
+    } else if (indovino < n){
+        console.log(indovino + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
+    } else if(indovino > n){
+        console.log(indovino + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
+    }
+
+    if(tentativi >= 5){
+        console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
+    }
+
+    userField.value = "";
 }
 
-let n = 0, tentativi = 0, indovinare;
-let userField, btn;
-n = generateRandomInteger(100);
-
+let guessBtn = document.querySelector("#guessBtn");
 guessBtn.addEventListener("click", handleClick);
