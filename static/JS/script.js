0eecbc06982bd76391d1033d6c42ddfb51f80fe8
Francesco00713 <kiccofranca13@gmail.com>
2024-10-13 19:31:15 +0200

    gioco corretto

--- static/JS/script.js
+++ static/JS/script.js
@@ -8,31 +8,31 @@ let tentativi = 0;
 const n = generateRandomInteger(0, 100);
 
 function handleClick(event){
-
-
     let userField = document.querySelector("#guess");
 
     if(userField === null || isNaN(userField.value) || userField.value === ""){
         console.log("Devi inserire un numero valido!");
-        return;
+    } else {
+        let indovino = parseInt(userField.value);
+
+        if (indovino < 0 || indovino > 100) {
+            console.log("Devi inserire un numero tra 0 e 100!");
+        } else {
+            
+            tentativi++;
+
+            if(indovino === n){
+                console.log("Complimenti hai vinto!");
+            } else if (indovino < n){
+                console.log(indovino + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
+            } else if(indovino > n){
+                console.log(indovino + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
+        }
+        if(tentativi >= 5){
+            console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
+        }
+        }
     }
-
-    let indovino = parseInt(userField.value);
-    tentativi++;
-
-    if(indovino === n){
-        console.log("Complimenti hai vinto!");
-        return;
-    } else if (indovino < n){
-        console.log(indovino + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
-    } else if(indovino > n){
-        console.log(indovino + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
-    }
-
-    if(tentativi >= 5){
-        console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
-    }
-
     userField.value = "";
 }
 
