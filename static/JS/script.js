85eccf78b56d019f07ed81a7313682c66d72f92a
Francesco00713 <kiccofranca13@gmail.com>
2024-10-20 22:18:35 +0200

    Aggiunto il codice css e la funzione innerHTML

--- static/JS/script.js
+++ static/JS/script.js
@@ -5,6 +5,7 @@ function generateRandomInteger(min = 0,  max = 100){
 }
 
 let tentativi = 0;
+let tentativo;
 const n = generateRandomInteger(0, 100);
 
 function handleClick(event){
@@ -22,14 +23,14 @@ function handleClick(event){
             tentativi++;
 
             if(indovino === n){
-                console.log("Complimenti hai vinto!");
+                let vittoria = document.querySelector("#vittoria").innerHTML ="Complimenti hai vinto!";
             } else if (indovino < n){
-                console.log(indovino + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
+                tentativo = document.querySelector("#tentativo").innerHTML = indovino + " E' un numero troppo piccolo, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!";
             } else if(indovino > n){
-                console.log(indovino + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!");
+                tentativo = document.querySelector("#tentativo").innerHTML = indovino + " E' un numero troppo grande, hai ancora " + (5-tentativi) + " tentativi. INSERISCI UN ALTRO NUMERO!";
         }
         if(tentativi >= 5){
-            console.log("Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n);
+            tentativo = document.querySelector("#tentativo").innerHTML = "Hai esaurito il numero di tentativi, hai perso! Il numero da indovinare era: " + n;
         }
         }
     }
